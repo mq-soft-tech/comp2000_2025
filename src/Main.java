4703c547d7363d9f2704224e6ab0dd5e62455f99
MQ-AndyZhan <wenchao.zhan@students.mq.edu.au>
2025-08-14 16:21:55 +1000

    task 3

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,17 @@ public class Main extends JFrame {
 
       @Override
       public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
+	    g.setColor(java.awt.Color.BLACK);
+	    g.drawRect(10, 10, 700, 700);
+      for (int x = 1; x < 20; x++)
+      {
+        g.drawLine(10, 10 + x * 35, 710, 10 + x * 35);
       }
+      for (int y = 1; y < 20; y++)
+      {
+        g.drawLine(10 + y * 35, 10, 10 + y * 35, 710);
+      }
+    }
     }
 
     private Main() {
@@ -35,4 +43,5 @@ public class Main extends JFrame {
         repaint();
       }
     }
+
 }
