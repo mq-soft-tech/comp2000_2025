9a474aacff20e3c157dd7f3e9e17df3110cadb83
MQUni47866276 <141313084+GnahcNevet2004@users.noreply.github.com>
2025-08-17 17:30:04 +1000

    Task3

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,36 @@
-import java.awt.Dimension;
-import java.awt.Graphics;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-
-
-    }
-
-    class Canvas extends JPanel {
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-      }
-
-      @Override
-      public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
-      }
-    }
-
-    private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
-    }
-
-    public void run() {
-      while(true) {
-        repaint();
-      }
-    }
+// for(int x=0; x<width; x+=width/36){
+//    for(int y=0; y<height; y+=height/36){
+// drawRect(x,y,width/36,height/36);
+//    }
+// }
+Main window = new Main();
 }
+class Canvas extends JPanel{
+public Canvas(){
+    setPreferredSize(new Dimension(720,720));
+}
+
+public void paint(Graphics g){
+    for (int i=10; i<710; i+=35){
+        for(int j=10; j<710; j+=35){
+          g.drawRect(i,j,35,35);  
+
+        }
+    }
+    
+}
+}
+private Main (){
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Canvas canvas = new Canvas();
+    this.setContentPane(canvas);
+    this.pack();
+    this.setVisible(true);
+}
+    }
+
