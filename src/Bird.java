6b01908ba2f6ff5ccc866a9560834cd0364b8491
WilliamMilne48231118 <william.milne@students.mq.edu.au>
2025-08-18 14:11:15 +1000

    Setting up for draw test

--- src/Bird.java
+++ src/Bird.java
@@ -3,13 +3,16 @@ import java.awt.Graphics;
 
 public class Bird extends Actor {
     Color colour;
+    Cell location;
 
     public Bird(){
         super();
         colour = Color.GREEN;
+        location = new Cell(20, 50);
     }
 
     public void paint(Graphics g){
-        
+        g.setColor(colour);
+        g.drawOval(location.x, location.y, 10, 10);
     }
 }
