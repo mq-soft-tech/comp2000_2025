20d34ef56869bf4402d4902d4686e1fa575c9b50
Jayden Nguyen <nchuong2962@gmail.com>
2025-08-29 00:14:52 +1000

    mod

--- src/Actor.java
+++ src/Actor.java
@@ -6,8 +6,10 @@ public abstract class Actor {
     Cell loc;
     
     public void paint(Graphics g){
-        g.setColor(Color.YELLOW); 
+        g.setColor(color);
         g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+        g.setColor(Color.GRAY);
+        g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
     } 
     
 }
