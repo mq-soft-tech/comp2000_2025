eafa3a4faa1c02338ff18fe4a7dd67e74e19ba42
Jayden Nguyen <nchuong2962@gmail.com>
2025-08-29 00:31:15 +1000

    m

--- src/Cell.java
+++ src/Cell.java
@@ -5,15 +5,11 @@ import java.awt.Rectangle;
 
 public class Cell extends Rectangle{
   // fields
-  int x;
-  int y;
   static int size = 35;
 
   // constructors
   public Cell(int x, int y) {
     super(x, y, size, size);
-    this.x = x;
-    this.y = y;
   }
 
   // methods
@@ -27,8 +23,7 @@ public class Cell extends Rectangle{
     g.setColor(Color.BLACK);
     g.drawRect(x, y, size, size);
   }
-
-  @Override
+  
   public boolean contains(Point p) {
     if(p != null) {
       return super.contains(p);
