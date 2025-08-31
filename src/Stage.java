83cff2fad9cee1232b764bc1926be25e8823398a
MQUni47866276 <141313084+GnahcNevet2004@users.noreply.github.com>
2025-08-31 21:30:34 +1000

    paint arraylist loop

--- src/Stage.java
+++ src/Stage.java
@@ -4,25 +4,28 @@ import java.util.*;
 
 public class Stage {
   Grid grid;
-  Actor cat;
-  Actor dog;
-  Actor bird;
-  ArrayList<Actor> actors = new ArrayList<Actor>();
-  actors.add(cat);
-  actors.add(dog);
-  actors.add(bird);
+  static Actor cat;
+  static Actor dog;
+  static Actor bird;
+ 
 
   public Stage() {
     grid = new Grid();
     cat = new Cat(grid.cellAtColRow(0, 0));
     dog = new Dog(grid.cellAtColRow(0, 15));
     bird = new Bird(grid.cellAtColRow(12, 9));
+    
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+    ArrayList<Actor> actors = new ArrayList<Actor>();
+    actors.add(cat);
+    actors.add(dog);
+    actors.add(bird);
+  for (int i=0; i<=actors.size(); i++){
+    actors.get(i).paint(g);
   }
+  } 
+
 }
