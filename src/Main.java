9baef4c23e1aaeddea36c0568a9044379d429792
MQUni47866276 <141313084+GnahcNevet2004@users.noreply.github.com>
2025-09-01 14:42:50 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import java.io.*;
+
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
       Main window = new Main();
