abbc354052463d09754d7717c5df1f4d7f70efd9
yuw4ns <161660991+yuw4ns@users.noreply.github.com>
2025-09-06 22:33:49 +1000

    Finished week 5 tasks
    Task 10, 11, and 12 are done.

--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,32 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class Stage {
   Grid grid;
-  Actor cat;
-  Actor dog;
-  Actor bird;
+  List<Actor> actors;
 
   public Stage() {
     grid = new Grid();
-    cat = new Cat(grid.cellAtColRow(0, 0));
-    dog = new Dog(grid.cellAtColRow(0, 15));
-    bird = new Bird(grid.cellAtColRow(12, 9));
+    actors = new ArrayList<Actor>();
+    actors.add(new Cat(grid.cellAtColRow(0, 0).get()));
+    actors.add(new Dog(grid.cellAtColRow(0, 15).get()));
+    actors.add(new Bird(grid.cellAtColRow(12, 9).get()));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
-    dog.paint(g);
-    bird.paint(g);
+    for(Actor a: actors) {
+      a.paint(g);
+    }
+    Optional<Cell> underMouse = grid.cellAtPoint(mouseLoc);
+    if(underMouse.isPresent()) {
+      Cell hoverCell = underMouse.get();
+      g.setColor(Color.DARK_GRAY);
+      g.drawString(String.valueOf(hoverCell.col) + String.valueOf(hoverCell.row), 740, 30);
+    }
   }
 }
