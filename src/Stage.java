55398566ddc82259d11f1b5110152a38f4e94069
MQUni47866276 <141313084+GnahcNevet2004@users.noreply.github.com>
2025-09-08 14:34:58 +1000

    Add new actor to test arraylist

--- src/Stage.java
+++ src/Stage.java
@@ -7,14 +7,14 @@ public class Stage {
   Actor cat;
   Actor dog;
   Actor bird;
- 
+  Actor TRex;
 
   public Stage() {
     grid = new Grid();
-    cat = new Cat(grid.cellAtColRow(0, 0));
-    dog = new Dog(grid.cellAtColRow(0, 15));
-    bird = new Bird(grid.cellAtColRow(12, 9));
-    
+    cat = new Cat(grid.cellAtColRow(5, 10));
+    dog = new Dog(grid.cellAtColRow(19, 17));
+    bird = new Bird(grid.cellAtColRow(15, 13));
+    TRex = new TRex(grid.cellAtColRow(2, 7));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
@@ -23,6 +23,7 @@ public class Stage {
     actors.add(0, cat);
     actors.add(1, dog);
     actors.add(2, bird);
+    actors.add(3, TRex);
   for (int i=0; i<actors.size(); i++){
     actors.get(i).paint(g);
   }
---
+++ src/TRex.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+
+public class TRex extends Actor {
+  public TRex(Cell inLoc) {
+    loc = inLoc;
+    color = Color.MAGENTA;
+  }
+}
