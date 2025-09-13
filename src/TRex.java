e3921e7a2d22a497a190d0a12e997aad7c57834e
MQUni47866276 <141313084+GnahcNevet2004@users.noreply.github.com>
2025-09-13 11:00:48 +1000

    actor polygon

--- src/TRex.java
+++ src/TRex.java
@@ -1,8 +1,26 @@
-import java.awt.Color;
+import java.awt.*;
+import java.util.*;
 
 public class TRex extends Actor {
   public TRex(Cell inLoc) {
     loc = inLoc;
     color = Color.MAGENTA;
+    display = new ArrayList<Polygon>();
+    Polygon ear1 = new Polygon();
+    ear1.addPoint(loc.x + 11, loc.y + 5);
+    ear1.addPoint(loc.x + 15, loc.y + 15);
+    ear1.addPoint(loc.x + 7, loc.y + 15);
+    Polygon ear2 = new Polygon();
+    ear2.addPoint(loc.x + 22, loc.y + 5);
+    ear2.addPoint(loc.x + 26, loc.y + 15);
+    ear2.addPoint(loc.x + 18, loc.y + 15);
+    Polygon face = new Polygon();
+    face.addPoint(loc.x + 5, loc.y + 10);
+    face.addPoint(loc.x + 35, loc.y + 10);
+    face.addPoint(loc.x + 35, loc.y + 30);
+    face.addPoint(loc.x + 5, loc.y + 30);
+    display.add(face);
+    display.add(ear1);
+    display.add(ear2);
   }
 }
